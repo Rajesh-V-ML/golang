3a393d13e8203a7a111d26099b5ad4a01a6c2217
rajeshv-maplelabs <rajesh.v@maplelabs.com>
2021-08-18 18:52:40 +0530

    get user input and multi return

--- learn_files/p4.go
+++ learn_files/p4.go
@@ -1,4 +1,7 @@
 //call by value
+//swap
+//multiple value return
+//input
 
 package main
 
@@ -7,4 +10,19 @@ import "fmt"
 func main() {
 	fmt.Printf("Hello")
 
+	var x, y int
+	fmt.Println("Enter X & Y")
+	fmt.Scanf("%d", &x)
+	fmt.Scanf("%d", &y)
+
+	fmt.Println(swap(x, y))
+
+}
+
+func swap(x, y int) (int, int) {
+	temp := x
+	x = y
+	y = temp
+
+	return x, y
 }
